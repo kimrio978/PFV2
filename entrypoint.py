523559f8eadb9860d2ea4147be17fff1680cd377
kimrio978 <sergiodelaossa11@gmail.com>
2022-11-16 00:28:00 -0500

    s

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 @app.route("/")
 def hello_world():
     return jsonify({"title":"prueba",
