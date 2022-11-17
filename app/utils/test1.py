0a8233b8126fdbc99abe0332ffef2a6b444ad1ca
kimrio978 <sergiodelaossa11@gmail.com>
2022-11-17 02:17:51 -0500

    asdffdas

--- app/utils/test1.py
+++ app/utils/test1.py
@@ -200,9 +200,7 @@ def func_lectura(month,day,ts):
 
         # # function to show the plot
         # plt.show()
-        plt.tight_layout()
         plt.savefig(strFile)
-        plt.show()
 
     except Error as ex:
         print("Error durante la conexion",ex)
